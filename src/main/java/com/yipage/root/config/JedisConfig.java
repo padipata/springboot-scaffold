cadd9b8cec169cc02794932f3405be7aebf23049
root <1208984024@qq.com>
2019-06-18 21:30:03 +0800

    fix jedisConfig

--- src/main/java/com/yipage/root/config/JedisConfig.java
+++ src/main/java/com/yipage/root/config/JedisConfig.java
@@ -9,15 +9,6 @@ import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
 public class JedisConfig extends CachingConfigurerSupport {
-    /**
-     * SpringSession  需要注意的就是redis需要2.8以上版本，然后开启事件通知，在redis配置文件里面加上
-     * notify-keyspace-events Ex
-     * Keyspace notifications功能默认是关闭的（默认地，Keyspace 时间通知功能是禁用的，因为它或多或少会使用一些CPU的资源）。
-     * 或是使用如下命令：
-     * redis-cli config set notify-keyspace-events Egx
-     * 如果你的Redis不是你自己维护的，比如你是使用阿里云的Redis数据库，你不能够更改它的配置，那么可以使用如下方法：在applicationContext.xml中配置
-     */
-
     @Value("${spring.redis.host}")
     private String host;
 
@@ -27,16 +18,16 @@ public class JedisConfig extends CachingConfigurerSupport {
     @Value("${spring.redis.timeout}")
     private int timeout;
 
-    @Value("${spring.redis.pool.max-active}")
+    @Value("${spring.redis.jedis.pool.max-active}")
     private int maxActive;
 
-    @Value("${spring.redis.pool.max-idle}")
+    @Value("${spring.redis.jedis.pool.max-idle}")
     private int maxIdle;
 
-    @Value("${spring.redis.pool.min-idle}")
+    @Value("${spring.redis.jedis.pool.min-idle}")
     private int minIdle;
 
-    @Value("${spring.redis.pool.max-wait}")
+    @Value("${spring.redis.jedis.pool.max-wait}")
     private long maxWaitMillis;
 
     @Bean
